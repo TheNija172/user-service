90dc90ac553e5f24f1e0da1844ca94bf48217e70
LomTik <vadim.bagy@Mail.ru>
2026-03-10 17:34:54 +0300

    Completed DAO layer

---
+++ src/main/java/org/example/userservice/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package org.example.userservice.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+}
